53fefe49359d2690755dd48b0f3e1cfde5cde409
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-03 18:24:18 +0530

    feat : Added : Checking Equality of Lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,17 +4,14 @@ namespace LineComparison
 {
     class Program
     {
-        public void LengthOfLine(int x1, int y1, int x2, int y2)
+        public double LengthOfLine(int x1, int y1, int x2, int y2)
         {
             //Operation
             double point1 = (x2 - x1);
             double point2 = (y2 - y1);
             double lengthOfLine = Math.Sqrt(Math.Pow(point1, 2) + Math.Pow(point2, 2));
 
-            //Display
-            Console.WriteLine(" Line Point 1 : " + point1 + " \n");
-            Console.WriteLine(" Line Point 2 : " + point2 + " \n");
-            Console.WriteLine(" Length of Line : " + lengthOfLine + " \n");
+            return lengthOfLine;
         }
         
         static void Main(string[] args)
@@ -24,9 +21,17 @@ namespace LineComparison
 
             //Object of a class
             Program lengthOfLine = new Program();
-            //call function to set value
-            lengthOfLine.LengthOfLine(5, 6, 7, 5);
             
+            //call function to set value
+            double line1 = lengthOfLine.LengthOfLine(5, 6, 7, 5);
+            double line2 = lengthOfLine.LengthOfLine(2, 8, 6, 12);
+
+            //Display Length of Lines
+            Console.WriteLine(" Length of Line 1 : " + line1 + " \n");
+            Console.WriteLine(" Length of Line 2 : " + line2 + " \n");
+
+            //Check Equality
+            Console.WriteLine("Length of Lines are Equal : " + line1.Equals(line2));
         }
     }
 }
